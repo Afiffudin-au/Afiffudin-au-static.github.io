5bfe617b4aa7acdba2f84c87ac45b8b99c6a4ea9
Afiffudin-au <afiffudin077@gmail.com>
2020-05-11 23:09:48 +0700

    add Translate x-y

--- script.js
+++ script.js
@@ -36,6 +36,30 @@ $(window).scroll(function(){
     });
   }
 
+  // Translate Effect
+  //sectin panel
+  if(Wscroll>$('.section-panel').offset().top-400){
+    $('.panel-translate-y').each(function(i){
+      setTimeout(function(){
+        $('.panel-translate-y').eq(i).addClass('show-translate');
+      },200 * i+1);
+    });
+   }
+
+
+  //features
+  if(Wscroll>$('.features').offset().top-200){
+   $('.workingspace-kiri, .content-quote-kanan').addClass('show-translate');
+  }
+  if(Wscroll>$('.features').offset().top+400){
+    $('.workingspace-kiri, .content-quote-kanan').removeClass('show-translate');
+   }
+
+
+
+
+
+
   //Windows Resize Or Mobile Device Setting
   if(Wscroll>$('.jumbotron').offset().top){
     if ($(window).width() <= 576) {  
